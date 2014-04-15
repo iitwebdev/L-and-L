7a3d19da846b188403f99517f8eb1f8949114af6
Anatoliy Lapin <mir.risen-li@mail.ru>
2014-04-15 15:26:39 +0600

    adding

---
+++ MyProject1/myproject1/views.py
@@ -0,0 +1,13 @@
+#coding: utf-8
+from pyramid.response import Response
+from pyramid.view import view_config
+
+from sqlalchemy.exc import DBAPIError
+
+@view_config(route_name='home', renderer='templates/home.jinja2')
+def my_view(request):
+    return {'project': 'MyProject1'}
+
+@view_config(route_name='user', renderer='templates/user.jinja2')
+def user_view(request):
+    return {'project': 'MyProject1'}
